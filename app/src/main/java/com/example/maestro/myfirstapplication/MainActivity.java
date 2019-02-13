8778d955b08efbb9d753fe922afa648dd8a0ebaf
Arrogance <fallenent6@gmail.com>
2019-02-13 16:33:57 +0000

    Added partner functionality

--- app/src/main/java/com/example/maestro/myfirstapplication/MainActivity.java
+++ app/src/main/java/com/example/maestro/myfirstapplication/MainActivity.java
@@ -2,7 +2,6 @@ package com.example.maestro.myfirstapplication;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
@@ -13,12 +12,16 @@ import android.widget.ImageView;
 public class MainActivity extends Activity {
 
     ImageButton returnButton;
+    ImageView user_Circle;
+    ImageView partner_Circle;
+    int phase = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ImageView user_Circle = findViewById(R.id.user_Circle);
+        user_Circle = findViewById(R.id.user_Circle);
+        partner_Circle= findViewById(R.id.partner_Circle);
 
         returnButton = findViewById(R.id.returnButton);
 
@@ -29,7 +32,42 @@ public class MainActivity extends Activity {
             }
         });
 
-        Animation expand = AnimationUtils.loadAnimation(this, R.anim.expand);
-            user_Circle.startAnimation(expand);
+        phase = 10;
+
+        if (phase == 10) {
+
+            fullExpand();
+            minExpand();
+
+        }
+
+        //phase = 1;
+
+        if (phase == 1){
+
+            minExpand();
+
+        }
+
     }
+
+    public MainActivity(){
+
+
+    }
+
+    public void fullExpand(){
+
+        Animation expand = AnimationUtils.loadAnimation(this, R.anim.expand);
+        user_Circle.startAnimation(expand);
+
+    }
+
+    public void minExpand(){
+
+        Animation expandMin = AnimationUtils.loadAnimation(this, R.anim.expand_min);
+        partner_Circle.startAnimation(expandMin);
+
+    }
+
 }
